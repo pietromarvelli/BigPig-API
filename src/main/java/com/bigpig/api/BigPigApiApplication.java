874982e4279c396931e3880324a112bd3fb25cb2
MarveeDev <marvee.social@gmail.com>
2025-03-05 13:20:03 +0100

    first up

---
+++ src/main/java/com/bigpig/api/BigPigApiApplication.java
@@ -0,0 +1,13 @@
+package com.bigpig.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigPigApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigPigApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bigpig/api/BigPigApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigpig.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigPigApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
