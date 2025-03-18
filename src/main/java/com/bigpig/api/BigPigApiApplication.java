b05dabea0f2b7838556db415ccf1bb64c991d5c2
Musooo <nicoloblack2@gmail.com>
2025-03-18 16:12:45 -0400

    boh si avvia ma forse c'è qualcosa che non va domani guardiamo meglio

--- src/main/java/com/bigpig/api/BigPigApiApplication.java
+++ src/main/java/com/bigpig/api/BigPigApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BigPigApiApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(BigPigApiApplication.class, args);
     }
